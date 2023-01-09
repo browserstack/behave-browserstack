b3f10eb9582f4926f0e6bdfa5005519236b95876
Ankit Singh <2501.ankit@gmail.com>
2023-01-09 09:47:52 +0530

    add: SDK flow for behave

--- features/environment.py
+++ features/environment.py
@@ -1,51 +1,13 @@
 from selenium import webdriver
-from browserstack.local import Local
-import os, json
-
-CONFIG_FILE = os.environ['CONFIG_FILE'] if 'CONFIG_FILE' in os.environ else 'config/single.json'
-TASK_ID = int(os.environ['TASK_ID']) if 'TASK_ID' in os.environ else 0
-
-with open(CONFIG_FILE) as data_file:
-    CONFIG = json.load(data_file)
-
-bs_local = None
-
-BROWSERSTACK_USERNAME = os.environ['BROWSERSTACK_USERNAME'] if 'BROWSERSTACK_USERNAME' in os.environ else CONFIG['user']
-BROWSERSTACK_ACCESS_KEY = os.environ['BROWSERSTACK_ACCESS_KEY'] if 'BROWSERSTACK_ACCESS_KEY' in os.environ else CONFIG['key']
-
-def start_local():
-    """Code to start browserstack local before start of test."""
-    global bs_local
-    bs_local = Local()
-    bs_local_args = { "key": BROWSERSTACK_ACCESS_KEY, "forcelocal": "true" }
-    bs_local.start(**bs_local_args)
-
-def stop_local():
-    """Code to stop browserstack local after end of test."""
-    global bs_local
-    if bs_local is not None:
-        bs_local.stop()
-
 
 def before_feature(context, feature):
-    desired_capabilities = CONFIG['environments'][TASK_ID]
-    desired_capabilities['browserstack.source'] = 'behave:sample-master:v1.0'
-    for key in CONFIG["capabilities"]:
-        if key not in desired_capabilities:
-            desired_capabilities[key] = CONFIG["capabilities"][key]
-
-    if "browserstack.local" in desired_capabilities and desired_capabilities["browserstack.local"]:
-        start_local()
-
+    desired_capabilities = {
+        'browserName': 'chrome'
+    }
     context.browser = webdriver.Remote(
         desired_capabilities=desired_capabilities,
-        command_executor="https://%s:%s@hub.browserstack.com/wd/hub" % (BROWSERSTACK_USERNAME, BROWSERSTACK_ACCESS_KEY)
+        command_executor="http://localhost:4444/wd/hub"
     )
 
 def after_feature(context, feature):
-    if context.failed is True:
-        context.browser.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": "At least 1 assertion failed"}}')
-    if context.failed is not True:
-        context.browser.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed", "reason": "All assertions passed"}}')
     context.browser.quit()
-    stop_local()
